c488ed0dd68968eafc4a1055b0779c792d1391cf
Imoh Precious <jaypee88830@gmail.com>
2021-06-27 03:44:53 +0100

    new changes

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+
+
+
+class App extends Component{
+  render() {
+     return (
+      <div>
+        <h1>You are currently logged (in/out)</h1>
+      </div>
+    )
+  }
+ 
+}
+
+
+
+export default App
+
---
+++ src/todosData.js
@@ -0,0 +1,31 @@
+const todosData = [
+
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Clean the floor",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Return the trash",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Buy akara and moi moi",
+        completed: false
+    },
+
+]
+
+export default todosData
